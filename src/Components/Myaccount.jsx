ec68af5060e12c57f592ab124f8b8a63e028d87f
Mdmahadi71 <habibalmasud79@gmail.com>
2024-08-15 09:09:26 -0700

    demo

--- src/Components/Myaccount.jsx
+++ src/Components/Myaccount.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from './Container'
+import { getDatabase, ref, onValue } from "firebase/database";
 
 const Myaccount = () => {
+  const db = getDatabase();
+  let [data , setdata] = useState([])
+
+
+
+  useEffect(() => {
+    const starCountRef = ref(db, 'user/');
+    onValue(starCountRef, (snapshot) => {
+        const arr = [];
+      snapshot.forEach((item)=>{
+        arr.push({...item.val()})
+      })
+      setdata(arr)
+    });
+  },[db])
+
+
+
   return (
-    <div>
+    <div className=' py-[100px]'>
       <Container>
-        <h2>habib</h2>
+        <div className=" w-[400px] mx-auto text-center bg-cyan-600">
+            {data.map((item)=>(
+              <div className="">
+              <h2>Name: {item.username}</h2>
+              <h2>Email: {item.email}</h2>
+          </div>
+            ))}
+            
+    
+          
+        </div>
       </Container>
     </div>
   )
